54b4ad6f047abf3d7ecf917fdd61623e4513430d
António Raimundo <aslro@iscte-iul.pt>
2023-07-06 00:28:48 +0100

    FenixGPT v0.0.1 - Proof-of-concept launched!

--- frontend/src/Hero.tsx
+++ frontend/src/Hero.tsx
@@ -2,20 +2,21 @@ export default function Hero({ className = '' }: { className?: string }) {
   return (
     <div className={`flex flex-col ${className}`}>
       <div className="mb-10 flex items-center justify-center">
-        <p className="mr-2 text-4xl font-semibold">DocsGPT</p>
+        <p className="mr-2 text-4xl font-semibold">FénixGPT</p>
         <p className="text-[27px]">🦖</p>
       </div>
       <p className="mb-3 text-center leading-6 text-black-1000">
-        Welcome to DocsGPT, your technical documentation assistant!
+        Bem-vindo ao FénixGPT, o teu assistente virtual de apoio à matrícula no
+        Iscte Sintra!
       </p>
       <p className="mb-3 text-center leading-6 text-black-1000">
-        Enter a query related to the information in the documentation you
-        selected to receive and we will provide you with the most relevant
-        answers.
+        Este assistente permite ajudar-te na matrícula e inscrição nas Unidades
+        Curriculares do curso a que te candidataste <br /> através do nosso
+        Sistema de Gestão Académica:{' '}
+        <a href={'https://fenix-mais.iscte-iul.pt/'}>Fénix+</a>.
       </p>
       <p className="mb-3 text-center leading-6 text-black-1000">
-        Start by entering your query in the input field below and we will do the
-        rest!
+        Começa por colocar a tua questão na caixa em branco abaixo.
       </p>
     </div>
   );
