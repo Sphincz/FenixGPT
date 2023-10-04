ace6c757cbbce129024a1fb87923441836a5804d
António Raimundo <aslro@iscte-iul.pt>
2023-10-04 19:56:16 +0100

    FenixGPT v0.3.1 - BETA + Updated App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,13 +7,13 @@ import { ActiveState } from './models/misc';
 
 export default function App() {
   //TODO : below media query is disjoint from tailwind. Please wire it together.
-  const [navState, setNavState] = useState<ActiveState>(
+  const [navState] = useState<ActiveState>(
     window.matchMedia('(min-width: 768px)').matches ? 'ACTIVE' : 'INACTIVE',
   );
 
   return (
     <div className="min-h-full min-w-full">
-      <Navigation navState={navState} setNavState={setNavState} />
+      <Navigation />
       <div
         // className={`transition-all duration-200 ${
         //   navState === 'ACTIVE' ? 'ml-0 md:ml-72 lg:ml-60' : 'ml-0 md:ml-16'
