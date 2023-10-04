5f28eb4c921f2922a4ce1bbf2a4a364730e54c93
António Raimundo <aslro@iscte-iul.pt>
2023-10-04 21:50:55 +0100

    FenixGPT v0.3.1 - BETA + Updated App.tsx and restored Navigation.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,46 @@
 import { Routes, Route } from 'react-router-dom';
-import Navigation from './Navigation';
 import Conversation from './conversation/Conversation';
 import About from './About';
-import { useState } from 'react';
+//import Navigation from './Navigation';
+import { useEffect, useState } from 'react';
 import { ActiveState } from './models/misc';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectSourceDocs,
+  setSelectedDocs,
+} from './preferences/preferenceSlice';
+import { Doc, getDocs } from './preferences/preferenceApi';
 
 export default function App() {
   //TODO : below media query is disjoint from tailwind. Please wire it together.
-  const [navState] = useState<ActiveState>(
+  const [navState, setNavState] = useState<ActiveState>(
     window.matchMedia('(min-width: 768px)').matches ? 'ACTIVE' : 'INACTIVE',
   );
 
+  const dispatch = useDispatch();
+  useSelector(selectSourceDocs);
+  useEffect(() => {
+    async function requestDocs() {
+      const data = await getDocs();
+      //console.log(data);
+      //dispatch(setSourceDocs(data));
+
+      // Auto-select 'merged.pdf' if available
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const mergedDoc = data.find((doc: Doc) => doc.name === 'merged.pdf');
+      if (mergedDoc) {
+        dispatch(setSelectedDocs(mergedDoc));
+      }
+    }
+
+    requestDocs();
+  }, [dispatch]);
+
   return (
     <div className="min-h-full min-w-full">
-      <Navigation />
+      {/*HIDE NAVIGATION*/}
+      {/*<Navigation navState={navState} setNavState={setNavState} />*/}
       <div
         // className={`transition-all duration-200 ${
         //   navState === 'ACTIVE' ? 'ml-0 md:ml-72 lg:ml-60' : 'ml-0 md:ml-16'
